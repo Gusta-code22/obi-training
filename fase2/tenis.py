e05a14ad57f3bfbd88e07e850024ec6fd3bceecb
Gusta-code22 <gustavobritomirandaxbox@gmail.com>
2026-08-05 19:32:49 -0300

    feat: add solution for OBI team balancing problem

---
+++ fase2/tenis.py
@@ -0,0 +1,20 @@
+# /*
+# Problema: Diferença entre Times
+# Origem: OBI - Treino
+# Status: Resolvido sozinho após estudar a ideia
+# Tempo: 25 minutos
+# Dificuldade pessoal: 6/10
+# Aprendi:
+# - Que existem apenas 3 combinações possíveis de duplas.
+# #
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+poss1 = abs((a + b) - (c + d))
+poss2 = abs((a + c) - (b + d))
+poss3 = abs((a + d) - (b + c))
+
+print(min(poss1, poss2, poss3))
